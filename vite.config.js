428656f2174bf7e61cfab977bf79ba9515e30c98
nazriel040608 <acmassirau@gmail.com>
2025-12-30 15:59:31 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
                 'resources/css/app.css',
-                'resources/css/style.css'
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
-});
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        emptyOutDir: true,
+    },
+})
