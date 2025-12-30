1a2ccdf090360fa16b8c067991cc629e576fd0b7
nazriel040608 <acmassirau@gmail.com>
2025-12-30 20:24:52 +0700

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Permission\Middleware\RoleMiddleware;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,12 +21,20 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-
     public function boot(): void
     {
+        // Set locale Carbon
         Carbon::setLocale('id');
-        $this->app['router']->aliasMiddleware('role', RoleMiddleware::class);
-        Paginator::useTailwind();
-    }
 
+        // Alias middleware role
+        $this->app['router']->aliasMiddleware('role', RoleMiddleware::class);
+
+        // Gunakan Tailwind untuk pagination
+        Paginator::useTailwind();
+
+        // Paksa HTTPS di production
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+    }
 }
