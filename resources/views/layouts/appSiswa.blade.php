885b0ec8b3e1bffb24b107e076f255ae404f09f5
nazriel040608 <acmassirau@gmail.com>
2025-12-30 19:00:00 +0700

    aa

--- resources/views/layouts/appSiswa.blade.php
+++ resources/views/layouts/appSiswa.blade.php
@@ -8,7 +8,7 @@
 
     {{-- CSS sidebar --}}
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/beranda.css', 'resources/css/kelas.css', 'resources/css/sidebar.css', 'resources/css/siswa.css', 'resources/css/styles.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     {{-- Bootstrap (untuk navbar siswa kamu) --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
