0cba8ee748b5d8b2a32f187231a2e691d2f331f5
nazriel040608 <acmassirau@gmail.com>
2025-12-30 17:17:01 +0700

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>SmartLab</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    @vite(['resources/css/app.css', 'resources/css/style.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <script src="https://unpkg.com/flowbite@1.3.4/dist/flowbite.js"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
