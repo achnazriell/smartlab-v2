aeed1bb1288a57d832b5933765f5f17eddb169fb
nazriel040608 <acmassirau@gmail.com>
2025-12-30 16:56:10 +0700

    aa

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-const mix = require('laravel-mix');
-
-mix
-    .sass('resources/sass/app.scss') // Jika menggunakan SASS
-    .js('resources/js/app.js')
-    .postCss('resources/css/app.css', [
-        require('tailwindcss'),
-    ]);
