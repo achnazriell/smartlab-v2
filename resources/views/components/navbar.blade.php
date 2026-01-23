caf6623ec4661a0a0b6a1deebf9b8c44fd6f013a
nazriel040608 <acmassirau@gmail.com>
2026-01-23 11:01:18 +0700

    fix hosting

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -21,7 +21,7 @@
     <div class="container mx-auto flex items-center justify-between">
         <!-- Kiri: Logo -->
         <div class="flex justify-start text-center">
-            <img src="image/SMART-LAB.png" alt="Logo" class="h-10 w-auto">
+            <img src="image/LogoSmartlab.webp" alt="Logo" class="h-10 w-auto">
         </div>
         <!-- buttons -->
         <div class="flex justify-end">
