9ee28a4f8cc8fd2adf624cb90191763130d496d3
nazriel040608 <acmassirau@gmail.com>
2026-01-30 04:48:45 +0700

    aa

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 
@@ -13,8 +12,10 @@ class RoleAndPermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create(['name' => 'Admin']);
-        Role::create(['name' => 'Guru']);
-        Role::create(['name' => 'Murid']);
+        $roles = ['Admin', 'Guru', 'Murid'];
+
+        foreach ($roles as $role) {
+            Role::firstOrCreate(['name' => $role, 'guard_name' => 'web']);
+        }
     }
 }
