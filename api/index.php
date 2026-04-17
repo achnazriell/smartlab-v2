4fcc2830336ed377578804ddc3b524d7cb4cbac9
nazriel040608 <acmassirau@gmail.com>
2026-04-17 08:48:57 +0700

    fixxxxxxxxxxx

--- api/index.php
+++ api/index.php
@@ -1,2 +1,17 @@
 <?php
+
+// Pastikan folder storage ada di /tmp agar tidak error Read-only
+$storageFolders = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/logs'
+];
+
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0777, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
