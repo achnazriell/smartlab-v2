893ff95978ac3e1875d009f6808474d0551a30ee
nazriel040608 <acmassirau@gmail.com>
2026-04-17 08:47:11 +0700

    fixxxx

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,8 +6,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -18,5 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->useStoragePath('/tmp/storage') // <--- TAMBAHKAN BARIS INI
+    ->booting(function ($app) {
+        $app->useStoragePath('/tmp/storage');
+    })
     ->create();
